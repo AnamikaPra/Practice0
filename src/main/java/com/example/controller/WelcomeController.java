9937b122945f9fde1c5ec676ada4e5ca2270c575
Anamika <anamika.dubai19@gmail.com>
2019-03-21 20:54:20 +0530

    comitting

--- src/main/java/com/example/controller/WelcomeController.java
+++ src/main/java/com/example/controller/WelcomeController.java
@@ -7,7 +7,7 @@ import com.example.demo.DemoApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.*;
